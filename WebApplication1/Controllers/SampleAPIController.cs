13e4d49123e9021043a7d983c76b25b870e4432e
khoinguyen0412 <nguyenkhoi2227@gmail.com>
2024-02-26 09:50:50 +0700

    Add House class

---
+++ WebApplication1/Controllers/SampleAPIController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+
+    [Route("api/HouseAPI")]
+    [ApiController]
+    public class SampleAPIController :ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<House> GetHouse()
+        {
+            return new List<House>
+            {
+                new(){ Id = 1, Name="Pool View"},
+                new(){ Id = 2, Name = "Hill View" }
+            };
+        }
+    }
+}
---
+++ WebApplication1/Models/House.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class House
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
